f96e773b5e771ad33d4dd699ebeeacbae557da85
wagner.rangel <wagner.rangel@exablack.com>
2022-01-12 19:18:55 -0300

    Inital commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="q-ma-md">
+    <div class="q-gutter-md">
+      <q-input v-model="name" label="Nome" :bla="true" />
+      <q-input v-model="password" type="password" label="Senha" />
+      <q-btn color="primary" label="Enviar" @click="login" :loading="isLoading"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      name: '',
+      password: '',
+      isLoading: false
+    }
+  },
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+    console.log(this.$moment().format('MMMM Do YYYY, h:mm:ss a'))
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeUnmount () {
+    console.log('beforeUnmount')
+  },
+  unmounted () {
+    console.log('unmounted')
+  },
+  methods: {
+    async login () {
+      try {
+        this.isLoading = !this.isLoading
+        await this.$store.dispatch('auth/AUTH_REQUEST', { name: this.name, password: this.password })
+        this.$router.push({ path: '/private' })
+        this.isLoading = !this.isLoading
+      } catch (error) {
+        this.isLoading = !this.isLoading
+      }
+    }
+  }
+}
+</script>
